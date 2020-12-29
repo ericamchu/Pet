44fc8ed47c26426e08055dd30ccf9641c97c3417
sureynix <moshe.heletz@gmail.com>
2020-12-29 14:53:43 -0800

    Got the top bar working and animal showing up. Going to work on the "Settings" button now on the menu, after making the menu show up.

---
+++ app/src/main/java/com/example/wireframe/CharacterActivity.java
@@ -0,0 +1,13 @@
+package com.example.wireframe;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CharacterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
